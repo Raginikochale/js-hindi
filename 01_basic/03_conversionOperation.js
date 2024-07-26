71279d4a16ada1082df026da634ce79c325324fb
Raginikochale <146700609+Raginikochale@users.noreply.github.com>
2024-07-26 06:03:06 +0000

    conversion of dataTypes

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,30 @@
+let score = "33abc"
+
+//console.log(typeof score)
+//console.log(typeof(score))
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber)
+//console.log(valueInNumber)
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = "Ragini"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn)
+
+// 1 => true; 0 => false
+// "" => false
+// "Ragoni" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber);
+ 
+//let string = "83"
+//console.log(string);
