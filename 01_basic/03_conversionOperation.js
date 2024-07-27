9f99cff69303473612742815860149cc7590d393
Raginikochale <146700609+Raginikochale@users.noreply.github.com>
2024-07-27 17:22:14 +0000

    conversion of dataTypes

--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -23,12 +23,8 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber);
- 
-//let string = "83"
-//console.log(string);
-
+//console.log(stringNumber)
+//console.log(typeof string)
 
 //*********************************Operations*******************************
 
