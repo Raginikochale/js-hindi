f78fce945222579ae53e855af66feaab0624fcc8
Raginikochale <146700609+Raginikochale@users.noreply.github.com>
2024-08-11 08:43:17 +0000

    Control Flow

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,29 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = "March"
+
+switch (month) {
+    case "Jan":
+        console.log("January");
+        break;
+    case "Feb":
+        console.log("Febuary");
+        break;
+    case "March":
+        console.log("March");
+        break;
+    case "April":
+        console.log("April");
+        break;
+
+    default:
+        console.log("defaul case match");
+        break;
+}
