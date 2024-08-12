ac4cc63f253abd41414eed1dbbcabbb31aabf9aa
Raginikochale <146700609+Raginikochale@users.noreply.github.com>
2024-08-12 11:38:00 +0000

    Higher order array loop

---
+++ 05_iterations/five.js
@@ -0,0 +1,44 @@
+const coding = ["js", "ruby", "java", "python", "cpp"]
+
+// coding.forEach(function (val) {
+//    console.log(val);
+// })
+
+
+// coding.forEach( (item) => {
+//     console.log(item)
+// })
+
+
+// function printMe(item){
+//     console.log(item);
+// }
+
+// coding.forEach(printMe)
+
+
+// coding.forEach((item, index, arr) => {
+//     console.log(item, index, arr);
+    
+// })
+
+
+const myCoding = [
+    {
+        languageName: "javascript",
+        languageFile: "js"
+    },
+    {
+        languageName: "java",
+        languageFile: "Notepad"
+    },
+    {
+        languageName: "python",
+        languageFile: "py"
+    }
+]
+
+myCoding.forEach( (item) => {
+    console.log(item.languageName);
+    
+})
